d59bbe8aa163b7ba16d48b42ae8167c2a5a2f326
timothy-choi <tchoi720@gmail.com>
2024-11-02 00:01:26 -0700

    adding some attributres to user auth model for google login

--- Backend/Users/Model.py
+++ Backend/Users/Model.py
@@ -7,6 +7,6 @@ db = SQLAlchemy()
 class Users(db.Model):
     id = db.Column(PG_UUID, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    username: db.Column(db.String, nullable=False)
-    email: db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     userAuth_id = db.Column(PG_UUID, foreign_key=True)
