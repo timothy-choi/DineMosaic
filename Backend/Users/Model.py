937dd52ad1d7a8ab7893d3ff6e51fe4304dde74b
timothy-choi <tchoi720@gmail.com>
2024-11-01 17:12:21 -0700

    starting model design for users service - forgot to add model work

---
+++ Backend/Users/Model.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+import uuid 
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    id = db.Column(PG_UUID, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    username: db.Column(db.String, nullable=False)
+    email: db.column(db.String, nullable=False)
