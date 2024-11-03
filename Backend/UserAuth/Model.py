28666bd87df87776b7c1f7141b2b71149584e74f
timothy-choi <tchoi720@gmail.com>
2024-11-02 23:27:40 -0700

    changing schemas with proper schema style

--- Backend/UserAuth/Model.py
+++ Backend/UserAuth/Model.py
@@ -1,14 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 import uuid 
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
 class UserAuth(db.Model):
-    id = db.Column(PG_UUID, primary_key=True)
-    user_id = db.Column(PG_UUID, foreign_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False) 
-    email = db.Column(db.String, unique=True, nullable=False)
-    google_id = db.Column(db.String, nullable=True)
-    auth_provider = db.Column(db.String, default="local")
+    __tablename__ = 'user_auth'
+
+    id = db.Column(PG_UUID(as_uuid=True), primary_key=True)
+    user_id = db.Column(PG_UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=True)
+    password = db.Column(db.String(100), nullable=True) 
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    google_id = db.Column(db.String(100), unique=True, nullable=True)
+    auth_provider = db.Column(db.String(20), default="local")
+
+
--- Backend/Users/Model.py
+++ Backend/Users/Model.py
@@ -1,12 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 import uuid 
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy import ForeignKey
+
 
 db = SQLAlchemy()
 
 class Users(db.Model):
-    id = db.Column(PG_UUID, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    userAuth_id = db.Column(PG_UUID, foreign_key=True)
+    _tablename_ = 'users'
+
+    id = db.Column(PG_UUID(as_uuid=True), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    userAuth_id = db.Column(PG_UUID(as_uuid=True), ForeignKey('user_auth.id'), nullable=False)
